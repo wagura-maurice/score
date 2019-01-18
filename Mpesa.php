cbc3c39de4011dee7a295f4025ee382b0efd8ac7
Wagura Maurice <wagura465@gmail.com>
2019-01-18 11:19:12 +0300

    mpesa sortring class init

---
+++ Mpesa.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is a simple PHP class for scoring MPesa transactions.
+ * 
+ * @package Mpesa Transactions Sorting
+ * @license http://mit.com/licence MIT LICENCE
+ * @author Maurice Wagura <wagura465@gmail.com>
+ */
+
+class Mpesa {
+	/**
+	 * The common part of scoring MPesa transactions.
+	 * @var string $BASELINE
+	 */
+	private $BASELINE;
+
+}
